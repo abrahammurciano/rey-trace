6791d3a81a16560743320ec0755b37f8ad8e32df
Abraham Murciano <abrahammurciano@gmail.com>
2021-03-17 02:31:53 +0200

    tests

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 An image renderer built with ray tracing technology.
 
+[Documentation](https://abrahammurciano.github.io/rey-trace/)
+
 ![Rey Palpatine](https://i.imgur.com/kQjRmOp.jpg)
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-import primitives.*;
+import primitives.Vector;
+import primitives.ZeroVectorException;
 import static java.lang.System.out;
-import static primitives.Util.*;
+import primitives.Util;
+import primitives.Point;
 
 /**
  * Test program for the 1st stage
@@ -18,8 +20,9 @@ public final class Main {
 
 		try { // test zero vector
 			new Vector(0, 0, 0);
-			out.println("ERROR: zero vector does not throw an exception");
-		} catch (Exception e) {
+			throw new AssertionError("ERROR: zero vector does not throw an exception");
+		} catch (ZeroVectorException e) {
+			out.println("Zero vector throws an exception: Passed");
 		}
 
 		Vector v1 = new Vector(1, 2, 3);
@@ -27,45 +30,52 @@ public final class Main {
 		Vector v3 = new Vector(0, 3, -2);
 
 		// test length..
-		if (!isZero(v1.squareLength() - 14))
-			out.println("ERROR: squareLength() wrong value");
-		if (!isZero(new Vector(0, 3, 4).length() - 5))
-			out.println("ERROR: length() wrong value");
+		if (!Util.equals(v1.squareLength(), 14))
+			throw new AssertionError("ERROR: squareLength() wrong value");
+		if (!Util.equals(new Vector(0, 3, 4).length(), 5))
+			throw new AssertionError("ERROR: length() wrong value");
+		out.println("Vector length check: Passed");
 
 		// test Dot-Product
-		if (!isZero(v1.dot(v3)))
-			out.println("ERROR: dot() for orthogonal vectors is not zero");
-		if (!isZero(v1.dot(v2) + 28))
-			out.println("ERROR: dot() wrong value");
+		if (!Util.isZero(v1.dot(v3)))
+			throw new AssertionError("ERROR: dot() for orthogonal vectors is not zero");
+		if (!Util.equals(v1.dot(v2), -28))
+			throw new AssertionError("ERROR: dot() wrong value");
+		out.println("Dot product check: Passed");
 
 		// test Cross-Product
 		try { // test zero vector
 			v1.cross(v2);
-			out.println("ERROR: cross() for parallel vectors does not throw an exception");
-		} catch (Exception e) {
+			throw new AssertionError(
+					"ERROR: cross() for parallel vectors does not throw an exception");
+		} catch (ZeroVectorException e) {
+			out.println("Cross product throws zero vector exception: Passed");
 		}
 		Vector vr = v1.cross(v3);
-		if (!isZero(vr.length() - v1.length() * v3.length()))
-			out.println("ERROR: cross() wrong result length");
-		if (!isZero(vr.dot(v1)) || !isZero(vr.dot(v3)))
-			out.println("ERROR: cross() result is not orthogonal to its operands");
+		if (!Util.equals(vr.length(), v1.length() * v3.length()))
+			throw new AssertionError("ERROR: cross() wrong result length");
+		if (!Util.isZero(vr.dot(v1)) || !Util.isZero(vr.dot(v3)))
+			throw new AssertionError("ERROR: cross() result is not orthogonal to its operands");
+		out.println("Cross product checks: Passed");
 
 		// test vector normalization vs vector length and cross-product
 		Vector v = new Vector(1, 2, 3);
 		Vector vCopyNormalize = v.normalized();
-		if (!isZero(vCopyNormalize.length() - 1))
-			out.println("ERROR: normalize() result is not a unit vector");
+		if (!Util.equals(vCopyNormalize.length(), 1))
+			throw new AssertionError("ERROR: normalize() result is not a unit vector");
 		Vector u = v.normalized();
 		if (u == v)
-			out.println("ERROR: normalized() function does not create a new vector");
+			throw new AssertionError("ERROR: normalized() function does not create a new vector");
+		out.println("Normalization check: Passed");
 
 		// Test operations with points and vectors
 		Point p1 = new Point(1, 2, 3);
 		if (!Point.ORIGIN.equals(p1.add(new Vector(-1, -2, -3))))
-			out.println("ERROR: Point + Vector does not work correctly");
+			throw new AssertionError("ERROR: Point + Vector does not work correctly");
 		if (!new Vector(1, 1, 1).equals(new Point(2, 3, 4).vectorFrom(p1)))
-			out.println("ERROR: Point - Point does not work correctly");
+			throw new AssertionError("ERROR: Point - Point does not work correctly");
+		out.println("Point and vector operations check: Passed");
 
-		out.println("If there were no any other outputs - all tests succeeded!");
+		out.println("All tests succeeded!");
 	}
 }
