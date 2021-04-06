3a4019cee30ddb7fec878a64672a64e14bf86813
eli <elilevin11@gmail.com>
2021-04-06 18:12:09 +0300

    Ray tests. with tabs :)

---
+++ src/test/primitives/RayTests.java
@@ -0,0 +1,23 @@
+package primitives;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test the functions of the ray class
+ *
+ * @author Abraham Murciano
+ * @author Eli Levin
+ */
+public class RayTests {
+
+	private final Ray r = new Ray(new Point(1,2,3), new Vector(3,2,1));
+
+	@Test
+	public void equals() {
+		Assert.assertEquals("Rays should be equal", 
+				r, new Ray(new Point(1,2,3), new Vector(3,2,1)));
+		Assert.assertNotEquals("Rays should not be equal",
+				r, new Ray(new Point(4,4,4), new Vector(3,2,1)));
+	}
+}
