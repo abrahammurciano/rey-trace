9be6af2d0af64625b96347d5c95539b243073e8d
Abraham Murciano <abrahammurciano@gmail.com>
2021-04-25 13:39:13 +0300

    finished geometries class

--- src/main/geometries/Geometries.java
+++ src/main/geometries/Geometries.java
@@ -1,15 +1,43 @@
 package geometries;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-
 import primitives.Point;
 import primitives.Ray;
 
 public class Geometries implements Intersectible {
 
-	private List<Intersectible> intersectibles = new LinkedList<>()
+	private List<Intersectible> intersectibles = new LinkedList<>();
+
+	/**
+	 * Construct a collection of geometries given an array of {@link Intersectible}s or given any number of
+	 * {@link Intersectible}s.
+	 *
+	 * @param intersectibles The {@link Intersectible}s to initialise the collection to.
+	 */
+	public Geometries(Intersectible... intersectibles) {
+		add(intersectibles);
+	}
+
+	/**
+	 * Add a single {@link Intersectible} to the collection.
+	 *
+	 * @param intersectible The {@link Intersectible} to add.
+	 */
+	public void add(Intersectible intersectible) {
+		intersectibles.add(intersectible);
+	}
+
+	/**
+	 * Add many {@link Intersectible}s to the collection.
+	 *
+	 * @param intersectibles The {@link Intersectible}s to add.
+	 */
+	public void add(Intersectible... intersectibles) {
+		for (Intersectible intersectible : intersectibles) {
+			add(intersectible);
+		}
+	}
 
 	@Override
 	public List<Point> intersect(Ray ray) {
