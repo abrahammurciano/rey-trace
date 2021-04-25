877dbff56715edea49c122021da338b01983aeca
Abraham Murciano <abrahammurciano@gmail.com>
2021-04-25 14:55:14 +0300

    finished eq partition tests

--- src/test/geometries/GeometriesTests.java
+++ src/test/geometries/GeometriesTests.java
@@ -1,5 +1,7 @@
 package geometries;
 
+import java.util.HashSet;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 import primitives.Point;
@@ -15,9 +17,23 @@ import primitives.Vector;
 public class GeometriesTests {
 	@Test
 	public void testIntersect() {
+		Vector i = new Vector(1, 0, 0);
+		Ray ray = new Ray(Point.ORIGIN, i);
+
+		// Equivalence partition tests
+
+		// Some shapes intersect
+		Geometries geometries =
+			new Geometries(new Sphere(new Point(2, 0, 0), 1), new Plane(new Point(0, 0, 1), i),
+				new Triangle(new Point(4, 0, 1), new Point(4, 1, -1), new Point(4, -1, -1)));
+		Assert.assertEquals("Wrong result when some shapes intersect",
+			new HashSet<>(geometries.intersect(ray)),
+			new HashSet<>(List.of(new Point(1, 0, 0), new Point(3, 0, 0), new Point(4, 0, 0))));
+
+		// Boundary values test
+
 		// Test intersections with empty geometries
-		Geometries geometries = new Geometries();
-		Ray ray = new Ray(Point.ORIGIN, new Vector(1, 0, 0));
+		geometries = new Geometries();
 		Assert.assertTrue("Empty geometries returned intersections.",
 			geometries.intersect(ray).isEmpty());
 	}
