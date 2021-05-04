35cf17cb97e913067056b34dd4255944585f1ace
eli <elilevin11@gmail.com>
2021-05-05 01:23:28 +0300

    Color class

--- src/main/primitives/Colour.java
+++ src/main/primitives/Colour.java
@@ -3,10 +3,12 @@ package primitives;
 /**
  * This class represents a digital colour, with the added functionality of adding and scaling colours.
  *
- * @author
- * @author
  */
 public class Colour {
+
+	// Internal variable for keeping track of the rgb values
+	private Vector rgb;
+
 	/**
 	 * Construct a new colour from the given red, green, and blue values.
 	 *
@@ -15,10 +17,11 @@ public class Colour {
 	 * @param blue  The blue value (0 to 255).
 	 */
 	public Colour(int red, int green, int blue) {
-		// TODO: implement
-		// Idea: we can use a VectorBase internally to store the values, thus allowing us to delegate add and scale to
-		// VectorBase (see why I wanted to call this one Vector? It's more general and we can use it everywhere. also
-		// colour is a vector).
+		this(new Vector(red, green, blue));
+	}
+
+	private Colour(Vector rgb) {
+		this.rgb = rgb;
 	}
 
 	/**
@@ -29,8 +32,7 @@ public class Colour {
 	 * @return The new colour which is the sum of the two colours.
 	 */
 	public Colour add(Colour colour) {
-		// TODO: implement
-		return null;
+		return new Colour(this.rgb.add(colour.rgb));
 	}
 
 	/**
@@ -41,21 +43,24 @@ public class Colour {
 	 * @return The new colour which is the result of the scale.
 	 */
 	public Colour scale(double factor) {
-		// TODO: implement
-		return null;
+		return new Colour(this.rgb.scale(factor));
 	}
 
 	/**
-	 * Returns the RGB value representing the color. (Bits 24-31 are alpha, 16-23 are red, 8-15 are green, 0-7 are
+	 * Returns the RGB value representing the color. (Bits 24-31 are blank since alpha in unused, 16-23 are red, 8-15 are green, 0-7 are
 	 * blue).
 	 *
 	 * @return the RGB value of the color.
 	 */
 	public int rgb() {
-		// BufferedImage, as used by ImageWriter, needs only the RGB value (int) to write a pixel. So no point exposing
-		// the actual java colour. That is just implementation detail (if we want to use it... (bitwise operations are
-		// fun)).
-		// TODO: implement
-		return 0;
+		return getInt(rgb.z) ^ (getInt(rgb.y) >> 8) ^ (getInt(rgb.x) >> 16);
+	}
+
+	private int getInt(double value) {
+		if (value < 0)
+			return 0;
+		if (value > 255)
+			return 255;
+		return (int) Math.round(value);
 	}
 }
