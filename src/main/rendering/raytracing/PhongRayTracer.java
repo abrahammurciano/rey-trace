8ff4e0971c00e5638c41bfaea519bf69fb0aa22e
Abraham Murciano <abrahammurciano@gmail.com>
2021-05-24 22:36:33 +0300

    finished phong

--- src/main/rendering/raytracing/PhongRayTracer.java
+++ src/main/rendering/raytracing/PhongRayTracer.java
@@ -4,12 +4,10 @@ import java.util.List;
 
 import geometries.Intersection;
 import lighting.LightSource;
-import math.compare.DoubleCompare;
 import primitives.Colour;
 import primitives.Material;
 import primitives.NormalizedVector;
 import primitives.Ray;
-import primitives.Vector;
 import scene.Scene;
 
 /**
@@ -21,60 +19,56 @@ import scene.Scene;
  */
 public class PhongRayTracer extends RayTracer {
 
-    /**
-     * Construct a new BasicRayTracer for the given scene.
-     *
-     * @param scene The scene to trace.
-     */
-    public PhongRayTracer(Scene scene) {
-        super(scene);
-    }
+	/**
+	 * Construct a new BasicRayTracer for the given scene.
+	 *
+	 * @param scene The scene to trace.
+	 */
+	public PhongRayTracer(Scene scene) {
+		super(scene);
+	}
 
-    @Override
-    public Colour trace(Ray ray) {
-        List<Intersection> intersections = scene.geometries.intersect(ray);
-        if (intersections.isEmpty()) {
-            return scene.background;
-        } else {
-            return colour(ray.closest(intersections));
-        }
-    }
+	@Override
+	public Colour trace(Ray ray) {
+		List<Intersection> intersections = scene.geometries.intersect(ray);
+		if (intersections.isEmpty()) {
+			return scene.background;
+		} else {
+			return colour(ray.closest(intersections));
+		}
+	}
 
-    private Colour colour(Intersection intersection) {
-        NormalizedVector v = scene.camera().position.vectorTo(intersection.point).normalized();
-        NormalizedVector n = intersection.normal();
-        double nv = n.dot(v);
-        if (DoubleCompare.eq(nv, 0)) {
-            return Colour.BLACK;
-        }
-        Material material = intersection.geometry.material;
-        Colour colour = material.colour.scale(1d / 255);
-        Colour diffuse = colour.scale(material.diffuse);
-        Colour ambient = colour.scale(material.ambient);
-        double specular = material.specular;
-        double shine = material.shine;
-        Colour result = material.emission.add(ambient);
-        for (LightSource light : scene.lights) {
-            NormalizedVector l = light.vectorTo(intersection.point);
-            double nl = n.dot(l);
-            if (DoubleCompare.gt(nl * nv, 0)) {
-                NormalizedVector reflected = l.add(n.scale(-2 * n.dot(l))).normalized();
-                double reflectedDotV = reflected.dot(v.reversed());
-                Colour intensity = light.colourAt(intersection.point);
-                result = result.add(diffuse(intensity, diffuse, nl)).add(specular(intensity, specular, reflectedDotV, shine));
-            }
-        }
-        return result;
-    }
+	private Colour colour(Intersection intersection) {
+		Material material = intersection.geometry.material;
+		Colour result = material.emission.add(scene.ambient.colour.scale(material.ambient));
+		for (LightSource light : scene.lights) {
+			result = result.add(localEffects(light, intersection));
+		}
+		return result;
+	}
 
-    private Colour diffuse(Colour intensity, Colour diffuse, double normalDotLight) {
-        intensity = intensity.scale(normalDotLight);
-        return new Colour(intensity.red() * diffuse.red(), intensity.green() * diffuse.green(),
-                intensity.blue() * diffuse.blue());
-    }
+	private Colour localEffects(LightSource light, Intersection intersection) {
+		NormalizedVector fromCamera = scene.camera().position.vectorTo(intersection.point).normalized();
+		NormalizedVector fromLight = light.vectorTo(intersection.point);
+		NormalizedVector normal = intersection.normal();
+		double normalDotCamera = normal.dot(fromCamera);
+		double normalDotLight = normal.dot(fromLight);
+		if (normalDotCamera * normalDotLight < 0) {
+			return Colour.BLACK; // camera and light are on the different sides
+		}
+		NormalizedVector reflected = fromLight.add(normal.scale(-2 * normal.dot(fromLight))).normalized();
+		double reflectedDotV = reflected.dot(fromCamera.reversed());
+		Colour colour = light.colourAt(intersection.point);
+		Material material = intersection.geometry.material;
+		return diffuse(colour, material, normalDotLight).add(specular(colour, material, reflectedDotV));
+	}
 
-    private Colour specular(Colour intensity, double specular, double reflectedDotV, double shine) {
-        return intensity.scale(specular * Math.pow(reflectedDotV, shine));
-    }
+	private Colour diffuse(Colour colour, Material material, double normalDotLight) {
+		return colour.scale(material.diffuse).scale(normalDotLight);
+	}
+
+	private Colour specular(Colour colour, Material material, double reflectedDotV) {
+		return colour.scale(material.specular).scale(Math.pow(reflectedDotV, material.shine));
+	}
 
 }
