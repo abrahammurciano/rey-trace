ff19122e213657b82e9567024a15f4e0f8ebcb86
Abraham Murciano <abrahammurciano@gmail.com>
2021-05-24 21:44:19 +0300

    added factors support to xml

---
+++ src/main/xml/factories/attribute/XmlFactorsFactory.java
@@ -0,0 +1,38 @@
+package xml.factories.attribute;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import primitives.Factors;
+import xml.XmlParserException;
+
+/**
+ * Constructs {@link Factors}s from XML attributes.
+ *
+ * @author Abraham Murciano
+ * @author Eli Levin
+ */
+public class XmlFactorsFactory implements XmlFactoryFromAttribute<Factors> {
+	private static final XmlDoubleFactory DOUBLE_FACTORY = new XmlDoubleFactory();
+
+	/**
+	 * Constructs a new {@link Factors} from an XML attribute string.
+	 *
+	 * @param attribute The XML attribute, in the form "r g b" where r, g, and b are integers between 0 and 255
+	 *                  representing the red, green, and blue values.
+	 * @return A new colour with the given values.
+	 * @throws XmlParserException if the input string was malformed.
+	 */
+	public Factors create(String attribute) {
+		try (Scanner scanner = new Scanner(attribute)) {
+			return new Factors(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble());
+		} catch (NoSuchElementException e) {
+			try {
+				double value = DOUBLE_FACTORY.create(attribute);
+				return new Factors(value, value, value);
+			} catch (XmlParserException __) {
+				throw new XmlParserException("Expected a string with one or three numbers but saw \"" + attribute + '"',
+					e);
+			}
+		}
+	}
+}
